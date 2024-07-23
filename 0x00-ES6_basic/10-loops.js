b6ce10dec348bd2bf983fab0b74f08186645cb0c
Cnnb01 <nyamambanyaguch@gmail.com>
2024-07-23 19:51:03 +0300

    es6 basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
     let newArr = []
     for (let idx of array) {
-    //   let value = array[idx];
       let newApnd = appendString + idx;
-      newArr.push(newApnd)
+      newArr.push(newApnd);
     }
   
     return newArr;
