899227aba1bada3d95112dc3dcb5c0de1de82183
Cnnb01 <nyamambanyaguch@gmail.com>
2024-07-25 00:03:53 +0300

    promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,27 @@
+import { uploadPhoto } from "./utils";
+import { createUser } from "./utils";
+
+function handleProfileSignup(){
+    const p1 = uploadPhoto().then((response)=>{
+        console.log(response.body)
+        return response
+    });
+
+    const p2 = createUser().then((response)=>{
+            console.log(response.firstName, response.lastName)
+            return response
+        });
+
+Promise.all([p1,p2]).then(values=>{
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+}).catch(()=>{
+    console.log("Signup system offline");
+});
+}
+export default handleProfileSignup;
+// uploadPhoto returns a response with the format
+
+// {
+//   status: 200,
+//   body: 'photo-profile-1',
+// }
