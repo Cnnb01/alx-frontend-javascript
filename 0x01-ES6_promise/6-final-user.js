2516f483e607203de7b75f99678c5013473fb8a9
Cnnb01 <nyamambanyaguch@gmail.com>
2024-07-27 00:18:58 +0300

    promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,2 +1,12 @@
 import signUpUser from "./4-user-promise";
-import
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName,fileName){
+    const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+    return Promise.allSettled(promises).then((values)=>
+    values.forEach((value)=>console.log({
+        status: value.status,
+        value: value
+})),
+);
+}
