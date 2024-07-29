03729d21cfa5ba9457c1f62703f6f6885694a7d2
Cnnb01 <nyamambanyaguch@gmail.com>
2024-07-29 23:49:08 +0300

    eslint errors

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,14 @@
 function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
-        console.log('Got a response from the API');
+      console.log('Got a response from the API');
       resolve({
         status: 200,
-        body: "success",
-        });
+        body: 'success',
+      });
     } else {
       reject(Error(' '));
     }
-
   });
-
 }
 export default handleResponseFromAPI;
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,15 @@
-import { uploadPhoto } from "./utils";
-import { createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup(){
-    const p1 = uploadPhoto().then((response) => {
-        console.log(response.body);
-        return response;
-    });
+function handleProfileSignup() {
+  const p1 = uploadPhoto().then((response) => {
+    console.log(response.body);
+    return response;
+  });
 
-    const p2 = createUser().then((response) => {
-            console.log(response.firstName, response.lastName);
-            return response;
-        });
+  const p2 = createUser().then((response) => {
+    console.log(response.firstName, response.lastName);
+    return response;
+  });
 
 Promise.all([p1,p2]).then(values=>{
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
