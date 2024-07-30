bffbec694ca5250f15051b496ccee5c11a27b563
Cnnb01 <nyamambanyaguch@gmail.com>
2024-07-30 19:27:57 +0300

    eslint fixes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -11,11 +11,11 @@ function handleProfileSignup() {
     return response;
   });
 
-Promise.all([p1,p2]).then(values=>{
+  Promise.all([p1, p2]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-}).catch(()=>{
-    console.log("Signup system offline");
-});
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
 }
 export default handleProfileSignup;
 // uploadPhoto returns a response with the format
