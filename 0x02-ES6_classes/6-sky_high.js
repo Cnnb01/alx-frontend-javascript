97450ac164aeb3192f998b4b52b68892ac30bd19
Cnnb01 <nyamambanyaguch@gmail.com>
2024-08-01 01:01:16 +0300

    classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,18 +1,18 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors){
-        super(sqft)
-        this._floors = floors
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get floors(){
-        return this._floors
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    evacuationWarningMessage() {
-        if (this.constructor !== Building) {
-          return`Evacuate slowly the ${this._floors} floors`;
-        }
+  evacuationWarningMessage() {
+    if (this.constructor !== Building) {
+      return `Evacuate slowly the ${this._floors} floors`;
     }
+  }
 }
