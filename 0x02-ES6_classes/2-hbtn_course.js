61e8cf6d7a21128bba84ad5ec100cbe59d8fcbd2
Cnnb01 <nyamambanyaguch@gmail.com>
2024-07-31 11:06:17 +0300

    classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,44 @@
-// export default class HolbertonCourse {
-//     constructor(name, length, students) {
-//         if(typeof name == "string")
-//             this._name = name;
-//         if(typeof length == 'number')
-//             this._number = number;
-//         if(Array.isArray(students))
-//             this._students = students;
-//     }
-// }
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+      throw new Error('Invalid attribute type');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get student() {
+    return this._students;
+  }
+
+  set student(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new Error('Students must be an array');
+    }
+    this._students = newStudents;
+  }
+}
